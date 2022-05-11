1dbc0dc37861556f5dbd6f67ce83ef74f67f3955
bnbn332 <bnbn332@naver.com>
2022-05-12 01:08:40 +0900

    가상코인거래소-공용컴포넌트

---
+++ do-it-react/src/doit-ui/withStyles.js
@@ -0,0 +1,12 @@
+// withStyles 하이어오더 컴포넌트
+// Theme.js파일을 연동
+
+import ThemedStyleSheet from 'react-with-styles/lib/ThemedStyleSheet';
+import aphroditeInterface from 'react-with-styles-interface-aphrodite';
+import { css, withStyles, withStylesPropTypes } from 'react-with-styles';
+import Theme from './Theme';
+
+ThemedStyleSheet.registerInterface(aphroditeInterface);
+ThemedStyleSheet.registerTheme(Theme);
+
+export { css, withStyles, withStylesPropTypes, ThemedStyleSheet };
