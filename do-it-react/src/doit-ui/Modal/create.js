c00eee46c8c1200bfb1ac3647be7ee7b30b96cc3
bnbn332 <bnbn332@naver.com>
2022-05-19 20:12:02 +0900

    .

--- do-it-react/src/doit-ui/Modal/create.js
+++ do-it-react/src/doit-ui/Modal/create.js
@@ -1,25 +1,30 @@
 // ModalProvider 생성 함수 추가
 
 import React, { PureComponent } from 'react';
-import Modal from '../Modal'; // 또는 import Modal from './index';
+import Modal from '../Modal'; // 혹은 import Modal from './index';
+
 import { Provider } from './context';
 
 export default function createModalProvider(ContentMap = {}) {
   return class ModalProvider extends PureComponent {
     constructor(props) {
       super(props);
+
       this.state = { showModal: false };
       this.handleClose = this.handleClose.bind(this);
       this.handleOpen = this.handleOpen.bind(this);
     }
+
     handleOpen(contentId, modalProps) {
       this.contentId = contentId;
       this.modalProps = modalProps;
       this.setState({ showModal: true });
     }
+
     handleClose() {
       this.setState({ showModal: false });
     }
+
     render() {
       const { children } = this.props;
       const { showModal } = this.state;
