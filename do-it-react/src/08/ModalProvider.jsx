6cd650ba3588a871c9a8a7ca595727cb39eb58c4
bnbn332 <bnbn332@naver.com>
2022-05-31 00:52:04 +0900

    회원가입

--- do-it-react/src/08/ModalProvider.jsx
+++ do-it-react/src/08/ModalProvider.jsx
@@ -1,8 +1,10 @@
 import createProvider from '../doit-ui/Modal/create';
-import { TRADE_COIN_MODAL } from './constants/modals';
+import { TRADE_COIN_MODAL, REGISTER_USER_MODAL } from './constants/modals';
 //import TradeCoinPage from './components/main/TradeCoinPage01';
 import TradeCoinPage from './components/main/TradeCoinPage';
+import RegisterPage from './components/signup/RegisterPage';
 
 export default createProvider({
   [TRADE_COIN_MODAL]: TradeCoinPage,
+  [REGISTER_USER_MODAL]: RegisterPage,
 });
